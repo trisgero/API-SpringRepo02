f3be736f84b8c789eb5f647efe168534d0ab18b6
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-16 22:38:12 +0200

    Add files via upload

---
+++ API-SpringRepo02/src/main/java/it/triexercise/APISpringRepo02/ApiSpringRepo02Application.java
@@ -0,0 +1,13 @@
+package it.triexercise.APISpringRepo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringRepo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringRepo02Application.class, args);
+	}
+
+}
